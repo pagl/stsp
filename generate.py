bd3cda9d65a8ab2af84141f293063617f98ff226
pagl <patryk.gliszczynski94@gmail.com>
2017-11-12 18:45:50 +0100

    Added graph generation functions

--- generate.py
+++ generate.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import seaborn as sns
 import shutil
 import subprocess
 
@@ -13,6 +11,8 @@ from argparse import ArgumentParser
 from collections import defaultdict, namedtuple
 from datetime import datetime
 from glob import glob
+
+from graphs import *
 
 """
 This app is used to run a set of automatic experiments and generate graphs.
@@ -32,11 +32,12 @@ OUTPUT_DIR = os.path.join("docs", "results")
 GRAPH_DIR = os.path.join("docs", "graphs")
 AGG_DATA_FILE = "agg_data.csv"
 AGG_DATA_FIELDS = ["algorithm", "instance", "run_id", "init_score",
-                   "final_score", "iterations", "steps", "run_time"]
+                   "final_score", "best_so_far", "iterations",
+                   "steps", "run_time", "quality", "cost"]
 
 ALGORITHMS = ["random", "heuristic", "greedy", "steepest"]
 EXPERIMENT_MAX_TIME = 1000
-EXPERIMENT_MAX_ITER = 5000
+EXPERIMENT_MAX_ITER = 400
 
 
 def clear_directory(directory):
@@ -51,8 +52,11 @@ def aggregate_results(clear_dir=True):
         instance, algorithm = os.path.split(filepath)[-1].rstrip(".res").split("_")
         data = open(filepath).read().strip().split("\n")
         for row in data[:-1]:
-            data_points.append([algorithm, instance] + row.split(","))
-        data_points.append(["optimal", instance, "-1", "-1", data[-1], "-1", "-1", "-1"])
+            row = row.split(",")
+            cost = float(row[5]) / float(row[2])
+            quality = 1 / cost
+            data_points.append([algorithm, instance] + row + [str(quality), str(cost)])
+        data_points.append(["optimal", instance, "-1", "-1", data[-1], "-1", "-1", "-1", "-1", "-1"])
     if clear_dir:
         clear_directory(OUTPUT_DIR)
     open(os.path.join(OUTPUT_DIR, AGG_DATA_FILE), "w").write("\n".join(map(",".join, data_points)))
@@ -84,52 +88,16 @@ def generate_results():
     aggregate_results()
 
 
-def save_figure(name):
-    plt.savefig(os.path.join(GRAPH_DIR, name))
-
-
-def get_facet_grid(data):
-    grid = sns.FacetGrid(data, size=5, col="instance", hue="instance",
-                         col_wrap=4, margin_titles=True, palette="deep",
-                         sharex=False, sharey=False)
-    grid.fig.tight_layout(w_pad=3)
-    return grid
-
-
-def algorithm_score_comparison(data):
-    data_min = data[data.groupby(["algorithm", "instance"])["final_score"].transform(min) == data["final_score"]]
-    data_max = data[data.groupby(["algorithm", "instance"])["final_score"].transform(max) == data["final_score"]]
-    data_no_random = data[data.algorithm != "random"]
-
-    # Lowest Score (Bar Plot)
-    get_facet_grid(data_min).map(sns.barplot, "algorithm", "final_score",
-                                 order=["random", "heuristic", "greedy", "steepest", "optimal"], palette=sns.color_palette("hls", 6))
-    save_figure("algorithm_score_comparison_bar_min.png")
-    # Highest Score (Bar Plot)
-    get_facet_grid(data_max).map(sns.barplot, "algorithm", "final_score",
-                                 order=["random", "heuristic", "greedy", "steepest", "optimal"], palette=sns.color_palette("hls", 6))
-    save_figure("algorithm_score_comparison_bar_max.png")
-    # Average Score (Bar Plot)
-    get_facet_grid(data).map(sns.barplot, "algorithm", "final_score", ci="sd", capsize=.05,
-                             order=["random", "heuristic", "greedy", "steepest", "optimal"],
-                             palette=sns.color_palette("hls", 6))
-    save_figure("algorithm_score_comparison_bar_avg.png")
-    # Letter Value Plot (without random algorithm)
-    get_facet_grid(data_no_random).map(sns.lvplot, "algorithm", "final_score",
-                                       order=["heuristic", "greedy", "steepest", "optimal"],
-                                       palette=sns.color_palette("hls", 4))
-    save_figure("algorithm_score_comparison_letval.png")
-    # Violin Plot (without random algorithm)
-    get_facet_grid(data_no_random).map(sns.violinplot, "algorithm", "final_score",
-                                       order=["heuristic", "greedy", "steepest", "optimal"],
-                                       palette=sns.color_palette("hls", 4))
-    save_figure("algorithm_score_comparison_violin.png")
-
-
 def generate_graphs():
-    clear_directory(GRAPH_DIR)
     data = pd.read_csv(os.path.join(OUTPUT_DIR, AGG_DATA_FILE))
-    algorithm_score_comparison(data)
+    clear_directory(GRAPH_DIR)
+
+    score_comparison(data, GRAPH_DIR)
+    time_comparison(data, GRAPH_DIR)
+    steps_comparison(data, GRAPH_DIR)
+    quality_comparison(data, GRAPH_DIR)
+    init_vs_final_score(data, GRAPH_DIR)
+    multi_start_score(data, GRAPH_DIR)
 
 
 def parse_args():
--- src/main/java/stsp/Experiment.java
+++ src/main/java/stsp/Experiment.java
@@ -26,14 +26,17 @@ public class Experiment {
     public void run() {
         long startTime = System.nanoTime();
         long iterStartTime, currentTime, iter = 0;
+        float bestScoreSoFar = Float.MAX_VALUE, scoreSum = 0;
         
         do {
             iterStartTime = System.nanoTime();
             Solver.Result result = this.solver.resolve();
             currentTime = System.nanoTime();
+            bestScoreSoFar = Math.min(bestScoreSoFar, result.score);
             logger.write(String.valueOf(iter++) + ",");
             logger.write(String.valueOf(result.initialScore) + ",");
             logger.write(String.valueOf(result.score) + ",");
+            logger.write(String.valueOf(bestScoreSoFar) + ",");
             logger.write(String.valueOf(result.iterations) + ",");
             logger.write(String.valueOf(result.steps) + ",");
             logger.writeln(String.valueOf((currentTime - iterStartTime) / 1e6));         
