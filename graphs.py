cbebbc20f5b019292f041c6fcd29bbc0b814dc14
pagl <patryk.gliszczynski94@gmail.com>
2017-12-04 12:47:09 +0100

    Added rotating labels on X axis

--- graphs.py
+++ graphs.py
@@ -16,7 +16,7 @@ TABUSEARCH = "tabusearch"
 
 order_all_with_opt = [RANDOM, HEURISTIC, GREEDY, STEEPEST, TABUSEARCH, ANNEALING, OPTIMAL]
 order_all = [RANDOM, HEURISTIC, GREEDY, STEEPEST, TABUSEARCH, ANNEALING]
-order_without_random_with_opt = [HEURISTIC, GREEDY, STEEPEST, TABUSEARCH, , ANNEALING, OPTIMAL]
+order_without_random_with_opt = [HEURISTIC, GREEDY, STEEPEST, TABUSEARCH, ANNEALING, OPTIMAL]
 order_without_heuristic = [RANDOM, GREEDY, STEEPEST]
 order_ls_sa_ts = [GREEDY, STEEPEST, TABUSEARCH, ANNEALING]
 order_ls = [GREEDY, STEEPEST]
@@ -37,6 +37,7 @@ def get_facet_grid(data):
     grid = sns.FacetGrid(data, size=5, col="instance", hue="instance",
                          col_wrap=3, margin_titles=True, palette="deep",
                          sharex=False, sharey=False)
+    [plt.setp(ax.get_xticklabels(), rotation=30) for ax in grid.axes.flat]
     grid.fig.tight_layout(w_pad=3)
     return grid
 
@@ -48,7 +49,7 @@ def score_comparison(data, output_dir):
     # Lowest Score (Bar Plot)
     get_facet_grid(data_min).map(sns.barplot, "algorithm", "final_score",
                                  order=order_all_with_opt,
-                                 palette=sns.color_palette("hls", 6))
+                                 palette=sns.color_palette("hls", 8))
     save_figure("score_comparison_bar_min", output_dir)
     save_data(data[["algorithm", "instance", "final_score"]].groupby(["algorithm", "instance"]).min(),
               "score_comparison_bar_min", output_dir)
@@ -56,7 +57,7 @@ def score_comparison(data, output_dir):
     # Highest Score (Bar Plot)
     get_facet_grid(data_max).map(sns.barplot, "algorithm", "final_score",
                                  order=order_all_with_opt,
-                                 palette=sns.color_palette("hls", 6))
+                                 palette=sns.color_palette("hls", 8))
     save_figure("score_comparison_bar_max", output_dir)
     save_data(data[["algorithm", "instance", "final_score"]].groupby(["algorithm", "instance"]).max(),
               "score_comparison_bar_max", output_dir)
@@ -64,7 +65,7 @@ def score_comparison(data, output_dir):
     # Average Score (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "final_score", ci="sd", capsize=.05,
                              order=order_all_with_opt,
-                             palette=sns.color_palette("hls", 6))
+                             palette=sns.color_palette("hls", 8))
     save_figure("score_comparison_bar_avg", output_dir)
     save_data(data[["algorithm", "instance", "final_score"]].groupby(["algorithm", "instance"]).mean(),
               "score_comparison_bar_avg", output_dir)
@@ -72,13 +73,13 @@ def score_comparison(data, output_dir):
     # Score (Letter Value Plot)
     get_facet_grid(data).map(sns.lvplot, "algorithm", "final_score",
                              order=order_without_random_with_opt,
-                             palette=sns.color_palette("hls", 4))
+                             palette=sns.color_palette("hls", 6))
     save_figure("score_comparison_letval", output_dir)
 
     # Score (Violin Plot)
     get_facet_grid(data).map(sns.violinplot, "algorithm", "final_score",
                              order=order_without_random_with_opt,
-                             palette=sns.color_palette("hls", 4))
+                             palette=sns.color_palette("hls", 6))
     save_figure("score_comparison_violin", output_dir)
 
 
@@ -86,13 +87,13 @@ def time_comparison(data, output_dir):
     # Average Time (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "run_time", ci="sd", capsize=.05,
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("time_comparison_bar_avg", output_dir)
 
     # Time (Letter Value Plot)
     get_facet_grid(data).map(sns.lvplot, "algorithm", "run_time",
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("time_comparison_letval", output_dir)
 
 
@@ -100,25 +101,25 @@ def steps_comparison(data, output_dir):
     # Average Steps (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "steps", ci="sd", capsize=.05,
                              order=order_ls_sa_ts,
-                             palette=sns.color_palette("hls", 3))
+                             palette=sns.color_palette("hls", 5))
     save_figure("steps_comparison_bar_avg", output_dir)
 
     # Steps (Violin Plot)
     get_facet_grid(data).map(sns.violinplot, "algorithm", "steps",
                              order=order_ls_sa_ts,
-                             palette=sns.color_palette("hls", 3))
+                             palette=sns.color_palette("hls", 5))
     save_figure("steps_comparison_violin", output_dir)
 
     # Average Iterations (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "iterations", ci="sd", capsize=.05,
                              order=order_ls_sa_ts,
-                             palette=sns.color_palette("hls", 3))
+                             palette=sns.color_palette("hls", 5))
     save_figure("iterations_comparison_bar_avg", output_dir)
 
     # Iterations (Violin Plot)
     get_facet_grid(data).map(sns.violinplot, "algorithm", "iterations",
                              order=order_ls_sa_ts,
-                             palette=sns.color_palette("hls", 3))
+                             palette=sns.color_palette("hls", 5))
     save_figure("iterations_comparison_violin", output_dir)
 
 
@@ -126,25 +127,25 @@ def quality_comparison(data, output_dir):
     # Average Quality (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "quality", ci="sd", capsize=.05,
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("quality_comparison_bar_avg", output_dir)
 
     # Quality (Letter Value Plot)
     get_facet_grid(data).map(sns.lvplot, "algorithm", "quality",
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("quality_comparison_letval", output_dir)
 
     # Average Cost (Bar Plot)
     get_facet_grid(data).map(sns.barplot, "algorithm", "cost", ci="sd", capsize=.05,
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("cost_bar_avg", output_dir)
 
     # Cost (Letter Value Plot)
     get_facet_grid(data).map(sns.lvplot, "algorithm", "cost",
                              order=order_all,
-                             palette=sns.color_palette("hls", 5))
+                             palette=sns.color_palette("hls", 7))
     save_figure("cost_comparison_letval", output_dir)
 
 
@@ -158,7 +159,7 @@ def init_vs_final_score(data, output_dir):
 
     grid = get_facet_grid(data)
     grid.map(sns.violinplot, "algorithm", "init_score",
-             order=order_ls, palette=sns.color_palette("hls", 3))
+             order=order_ls, palette=sns.color_palette("hls", 5))
     grid.map(sns.violinplot, "algorithm", "final_score",
              order=order_ls, palette="muted")
     grid.set(ylabel="score")
